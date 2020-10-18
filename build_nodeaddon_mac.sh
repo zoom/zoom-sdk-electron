9c132c67c2cffda3a7dc56392d1abff36aac9051
Vijay Suryawanshi <vijay@classpro.in>
2020-10-18 20:59:44 +0530

    feat: use as node module

--- build_nodeaddon_mac.sh
+++ build_nodeaddon_mac.sh
@@ -1,23 +1,23 @@
+cd $(dirname $0)
 
-cd `dirname $0`
-if [ -d "./demo/node_modules" ];then
-cd ./demo
-npm run-script postinstall-mac
+if [ -d "../../node_modules" ]; then
+  cd ../../
+  yarn run postinstall-mac
 else
-cd ./demo
-npm install
-npm run-script postinstall-mac
+  cd ../../
+  yarn install
+  yarn run postinstall-mac
 fi
 
 version=$(electron --version)
-if [ $? -ne  0 ];then
-echo "build fail ,electron not install"
-exit
+
+if [ $? -ne 0 ]; then
+  echo "build fail ,electron not install"
+  exit
 else
-cd ../
-sed -i "" 's/8.2.4/'${version#*v}'/g' ./demo/package.json
-node-gyp rebuild --target=${version#*v}  --dist-url=https://atom.io/download/electron
+  cd ./node_modules/zoom-sdk-electron
+  sed -i "" 's/8.2.4/'${version#*v}'/g' ../../package.json
+  node-gyp rebuild --target=${version#*v} --dist-url=https://atom.io/download/electron
 fi
-cp -Rf ./build/Release/zoomsdk.node  ./sdk/mac && cp -Rf ./build/Release/zoomsdk_render.node  ./sdk/mac
 
-
+cp -Rf ./build/Release/zoomsdk.node ./sdk/mac && cp -Rf ./build/Release/zoomsdk_render.node ./sdk/mac
